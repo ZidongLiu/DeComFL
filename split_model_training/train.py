811599b1704027170d4a989b9e94b5602f58f660
ZidongLiu <z.liu@insidepetroleum.com>
2024-04-07 14:48:56 -0700

    adjust ruler to 90; and flake8 error to use 88 as line length

--- split_model_training/train.py
+++ split_model_training/train.py
@@ -3,4 +3,6 @@ from shared.model_helpers import Net
 
 model = Net()
 
-mnist_train = torchvision.datasets.MNIST(root='\data', train=True, transform=None, download=True)
+mnist_train = torchvision.datasets.MNIST(
+    root="\data", train=True, transform=None, download=True
+)
