687b357727a432312f2e782eec22ea8ef4f74c6a
RogerRogerUSC <118097091+RogerRogerUSC@users.noreply.github.com>
2024-04-11 19:39:25 -0700

    Modify some default values

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ import argparse
 # Parameters
 def get_params(dataset: str):
     parser = argparse.ArgumentParser(description=f"PyTorch training")
-    parser.add_argument("--train-batch-size", type=int, default=128)
+    parser.add_argument("--train-batch-size", type=int, default=256)
     parser.add_argument("--test-batch-size", type=int, default=1000)
     parser.add_argument("--lr", type=float, default=1e-4, help="Learning rate")
-    parser.add_argument("--epoch", type=int, default=10)
-    parser.add_argument("--grad-estimate-method", type=str, default="forward")
+    parser.add_argument("--epoch", type=int, default=500)
+    parser.add_argument("--grad-estimate-method", type=str, default="middle")
     parser.add_argument("--mu", type=float, default=1e-4)
-    parser.add_argument("--eval-iteration", type=int, default=5000)
-    parser.add_argument("--train_update_iteration", type=int, default=100)
+    parser.add_argument("--eval-iteration", type=int, default=150)
+    parser.add_argument("--train-update-iteration", type=int, default=100)
 
     # Rarely change
     parser.add_argument("--seed", type=int, default=365, help="random seed")
