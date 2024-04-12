78de322643ad25a2f58be8e871fdf176b2ef92ea
ZidongLiu <z.liu@insidepetroleum.com>
2024-04-12 16:56:00 -0700

    clean up random_gradient_estimator

--- gradient_estimators/random_gradient_estimator.py
+++ gradient_estimators/random_gradient_estimator.py
@@ -1,8 +1,6 @@
 import torch
 from torch.nn import Parameter
-from torch import Tensor
-from typing import Iterator
-from shared.metrics import TensorMetric
+from typing import Iterator, Optional
 
 from enum import Enum
 
@@ -16,15 +14,22 @@ class RandomGradientEstimator:
 
     def __init__(
         self,
-        params: Iterator[Parameter],
+        model,
+        parameters: Optional[Iterator[Parameter]] = None,
         mu=1e-3,
-        n_perturbation=1,
+        num_pert=1,
         grad_estimate_method: GradEstimateMethod = GradEstimateMethod.central.value,
+        normalize_perturbation: bool = False,
     ):
-        self.params_list: list[Parameter] = list(params)
-        self.params_shape: list[Tensor] = [p.shape for p in self.params_list]
+        self.model = model
+        if parameters is None:
+            parameters = model.parameters()
+        self.parameters_list: list[Parameter] = list(parameters)
+        self.total_dimensions = sum([p.numel() for p in self.parameters_list])
+
         self.mu = mu
-        self.n_perturbation = n_perturbation
+        self.num_pert = num_pert
+        self.normalize_perturbation = normalize_perturbation
 
         self.grad_estimate_method = grad_estimate_method
         self.method_func_dict = {
@@ -32,91 +37,57 @@ class RandomGradientEstimator:
             GradEstimateMethod.central.value: self._central_method,
         }
 
-    def _params_list_set_(self, to_set: list[torch.Tensor]):
-        if (not isinstance(to_set, list)) or (not len(to_set) == len(self.params_list)):
-            raise Exception("Current to_set does not match controlled parameters")
+    def generate_perturbation_norm(self) -> torch.Tensor:
+        p = torch.randn(self.total_dimensions)
+        if self.normalize_perturbation:
+            return p / torch.norm(p)
+        else:
+            return p
 
-        for p, set_value in zip(self.params_list, to_set):
-            p.set_(set_value)
+    def perturb_model(self, perturb: torch.Tensor, *, alpha: float | int = 1) -> None:
+        start = 0
+        for p in self.parameters_list:
+            _perturb = perturb[start : (start + p.numel())]
+            p.add_(_perturb.view(p.shape), alpha=alpha)
+            start += p.numel()
 
-    def _generate_perturbation(self):
-        return [torch.randn(shape) for shape in self.params_shape]
+    def put_grad(self, grad: torch.Tensor) -> None:
+        start = 0
+        for p in self.parameters_list:
+            p.grad = grad[start : (start + p.numel())].view(p.shape)
+            start += p.numel()
 
-    def _forward_method(self, batch_inputs, labels, model, criterion):
-        start_params = [p.clone() for p in self.params_list]
-        start_pred = model(batch_inputs)
-        start_loss = criterion(start_pred, labels)
-
-        running_grads = [
-            TensorMetric(f"running_grad_{i}") for i in range(len(self.params_list))
-        ]
-
-        for i in range(self.n_perturbation):
-            perturbation = self._generate_perturbation()
-            perturbed_params = [
-                p + self.mu * perturb
-                for (p, perturb) in zip(start_params, perturbation)
-            ]
-            self._params_list_set_(perturbed_params)
-            perturbed_pred = model(batch_inputs)
-            perturbed_loss = criterion(perturbed_pred, labels)
-            perturbation_direction_grad = (perturbed_loss - start_loss) / self.mu
-
-            for param_tensor_idx, running_grad in enumerate(running_grads):
-                running_grad.update(
-                    perturbation_direction_grad * perturbation[param_tensor_idx]
-                )
-
-        grad = [running_grad.avg for running_grad in running_grads]
-        return start_params, grad
-
-    def _central_method(self, batch_inputs, labels, model, criterion):
-        start_params = [p.clone() for p in self.params_list]
-        running_grads = [
-            TensorMetric(f"running_grad_{i}") for i in range(len(self.params_list))
-        ]
-
-        for i in range(self.n_perturbation):
-            perturbation = self._generate_perturbation()
-            # forward perturbation
-            forward_perturbed_params = [
-                p + self.mu * perturb
-                for (p, perturb) in zip(start_params, perturbation)
-            ]
-            self._params_list_set_(forward_perturbed_params)
-            forward_perturbed_pred = model(batch_inputs)
-            forward_perturbed_loss = criterion(forward_perturbed_pred, labels)
-
-            # backward perturbation
-            backward_perturbed_params = [
-                p - self.mu * perturb
-                for (p, perturb) in zip(start_params, perturbation)
-            ]
-            self._params_list_set_(backward_perturbed_params)
-            backward_perturbed_pred = model(batch_inputs)
-            backward_perturbed_loss = criterion(backward_perturbed_pred, labels)
-
-            # update perturbation gradient
-            perturbation_direction_grad = (
-                forward_perturbed_loss - backward_perturbed_loss
-            ) / (2 * self.mu)
-
-            for param_tensor_idx, running_grad in enumerate(running_grads):
-                running_grad.update(
-                    perturbation_direction_grad * perturbation[param_tensor_idx]
-                )
-
-        grad = [running_grad.avg for running_grad in running_grads]
-        return start_params, grad
-
-    def estimate_and_assign_grad(self, batch_inputs, labels, model, criterion):
+    def compute_grad(self, batch_inputs, labels, criterion) -> None:
         estimation_method = self.method_func_dict[self.grad_estimate_method]
+        grad = estimation_method(batch_inputs, labels, criterion)
 
-        start_params, grad = estimation_method(batch_inputs, labels, model, criterion)
+        self.put_grad(grad)
 
-        # reset parameters to initial state
-        self._params_list_set_(start_params)
+    def _forward_method(self, batch_inputs, labels, criterion):
+        grad = 0
+        initial_loss = criterion(self.model(batch_inputs), labels)
+        for _ in range(self.num_pert):
+            pb_norm = self.generate_perturbation_norm()  # TODO add random seed
 
-        # assign grad to parameters
-        for i, p in enumerate(self.params_list):
-            p.grad = grad[i]
+            self.perturb_model(pb_norm, alpha=self.mu)
+            pert_plus_loss = criterion(self.model(batch_inputs), labels)
+            self.perturb_model(pb_norm, alpha=-self.mu)  # Restore model
+
+            dir_grad = (pert_plus_loss - initial_loss) / self.mu
+            grad += dir_grad * pb_norm
+        return grad / self.num_pert
+
+    def _central_method(self, batch_inputs, labels, criterion):
+        grad = 0
+        for _ in range(self.num_pert):
+            pb_norm = self.generate_perturbation_norm()  # TODO add random seed
+
+            self.perturb_model(pb_norm, alpha=self.mu)
+            pert_plus_loss = criterion(self.model(batch_inputs), labels)
+            self.perturb_model(pb_norm, alpha=-2 * self.mu)
+            pert_minus_loss = criterion(self.model(batch_inputs), labels)
+            self.perturb_model(pb_norm, alpha=self.mu)  # Restore model
+
+            dir_grad = (pert_plus_loss - pert_minus_loss) / (2 * self.mu)
+            grad += dir_grad * pb_norm
+        return grad / self.num_pert
