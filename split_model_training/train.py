ccdb84e889c9e957a3c072ee17a6caeb7e5ec749
ZidongLiu <z.liu@insidepetroleum.com>
2024-04-12 16:00:04 -0700

    remove redundant files

--- split_model_training/train.py
+++
@@ -1,8 +0,0 @@
-import torchvision
-from shared.model_helpers import Net
-
-model = Net()
-
-mnist_train = torchvision.datasets.MNIST(
-    root="\data", train=True, transform=None, download=True
-)
