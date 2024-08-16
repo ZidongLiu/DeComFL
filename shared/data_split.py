0b15d9c0e760c7c3d771ca470a98f57a09e619ff
ybc1991 <bichengying@gmail.com>
2024-08-15 23:09:55 -0700

    Add balance option

--- shared/data_split.py
+++ shared/data_split.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import torch
 import math
 import contextlib
@@ -5,32 +7,41 @@ import numpy as np
 from torch.utils.data.dataset import Subset
 
 
-def get_dirichlet_split_indexes(labels: list[int], num_split, alpha):
+def get_dirichlet_split_indexes(
+    labels: list[int], num_split: int, alpha: float, balance_approach: bool = True
+) -> list[list[int]]:
+    """Given a list of labels and split it num_splits that follows Dirichlet(alpha*p) distribution.
+    where p is the original class distribution in labels.
+
+    Args:
+        labels: A list of lables.
+        num_split: Number of the split.
+        alpha: A float number to indicate how non-iid the split it is. When alpha -> infinite, the
+            returned index is more homogeneous. When alpha -> 0, the returned index is more hetero.
+        balance_approach: If true, make sure all the clients has the same number of data. But it
+            may fail if too heterogeneous distribution is required.
+
+    Returns:
+        A list containing `num_split` sub-list.
+    """
     n_data = len(labels)
     if num_split == 1:
         return [list(range(n_data))]
-        
-    label_indices_dict = {}
+
+    label_indices_dict = defaultdict(list)
     for i, label in enumerate(labels):
-        if label in label_indices_dict:
-            label_indices_dict[label].append(i)
-        else:
-            label_indices_dict[label] = [i]
-    
+        label_indices_dict[label].append(i)
+
     unique_labels = list(label_indices_dict.keys())
     n_labels = len(unique_labels)
     if not sorted(unique_labels) == list(range(n_labels)):
-        raise ValueError(
-            "Please re-map the labels of dataset into "
-            "0 to num_classes-1 integers."
-        )
+        raise ValueError("Please re-map the labels of dataset into " "0 to num_classes-1 integers.")
 
     data_class_prob = np.array([len(label_indices_dict[i]) for i in range(n_labels)]) / n_data
     # This is a matrix with dimension "num_split * num_classes"
     full_class_prob = np.random.dirichlet(alpha * data_class_prob, num_split)
 
     split_label_idx = [[] for _ in range(num_split)]
-    
     for label, label_indices in label_indices_dict.items():
         cum_prob = np.cumsum(full_class_prob[:, label])
         normalized_cum_prob = (cum_prob / cum_prob[-1]).tolist()
@@ -41,6 +52,19 @@ def get_dirichlet_split_indexes(labels: list[int], num_split, alpha):
             my_index_start = math.floor(start_prob * num_label_indices)
             my_index_end = math.floor(end_prob * num_label_indices)
             split_label_idx[rank].extend(label_indices[my_index_start:my_index_end])
+
+    if balance_approach:
+        expected_length = len(labels) // num_split
+        diff_per_rank = [len(v) - expected_length for v in split_label_idx]
+        extra_labels = []
+        for rank in np.argsort(diff_per_rank)[::-1]:  # from the client has most extra labels
+            if diff_per_rank[rank] > 0:
+                np.random.shuffle(split_label_idx[rank])
+                extra_labels.extend(split_label_idx[rank][-diff_per_rank[rank] :])
+                split_label_idx[rank] = split_label_idx[rank][: -diff_per_rank[rank]]
+            elif diff_per_rank[rank] < 0:
+                split_label_idx[rank].extend(extra_labels[: abs(diff_per_rank[rank])])
+                extra_labels = extra_labels[abs(diff_per_rank[rank]) :]
 
     return [sorted(v) for v in split_label_idx]
 
@@ -62,7 +86,4 @@ def dirichlet_split(dataset, labels, num_split, alpha, random_seed):
     with temp_np_seed(random_seed):
         split_indexes = get_dirichlet_split_indexes(labels, num_split, alpha)
 
-    return [
-        Subset(dataset, split_indexes[i])
-        for i in range(num_split)
-    ]
+    return [Subset(dataset, split_indexes[i]) for i in range(num_split)]
