8689d9337fa5e5c8f8ac382526e93271278638c0
Zidong Liu <z.liu@insidepetroleum.com>
2024-09-01 20:11:00 -0500

    Security (#27)
    Co-authored-by: RogerRogerUSC <118097091+RogerRogerUSC@users.noreply.github.com>
    Co-authored-by: yang.5952 <yang.5952@ece-cnc197130s.coeit.osu.edu>
    Co-authored-by: ybc1991 <bichengying@gmail.com>
    
    @RogerRogerUSC will fix unit test and pass byzantine args explicitly to Cezo_server instead of passing whole args in this issue, https://github.com/ZidongLiu/FedDisco/issues/42

--- cezo_fl/server.py
+++ cezo_fl/server.py
@@ -1,13 +1,14 @@
+from __future__ import annotations
 import random
 import torch
 from typing import Any, Iterable, Sequence
 from collections import deque
-
 from cezo_fl.shared import CriterionType, update_model_given_seed_and_grad
-from cezo_fl.client import AbstractClient
-from cezo_fl.run_client_jobs import execute_sampled_clients
+from cezo_fl.client import LocalUpdateResult, AbstractClient
 from shared.metrics import Metric
 from gradient_estimators.random_gradient_estimator import RandomGradientEstimator as RGE
+from byzantine.aggregation import mean, median, trim, krum
+from byzantine.attack import no_byz, gaussian_attack, sign_attack, trim_attack, krum_attack
 
 
 class SeedAndGradientRecords:
@@ -56,6 +57,42 @@ class SeedAndGradientRecords:
         ]
 
 
+# TODO fix the multithreading issue (see https://github.com/ZidongLiu/FedDisco/issues/30)
+def parallalizable_client_job(
+    client: AbstractClient,
+    pull_seeds_list: Sequence[Sequence[int]],
+    pull_grad_list: Sequence[Sequence[torch.Tensor]],
+    local_update_seeds: Sequence[int],
+    server_device: torch.device,
+) -> LocalUpdateResult:
+    """
+    Run client pull and local update in parallel.
+    This function is added to make better use of multi-gpu set up.
+    Each client can be deployed to a separate gpu. Thus we can run all clients in parallel.
+
+    Note:
+    This function also make sure the data passed to/from client are converted to correct device.
+    We should only do cross device operation here
+    """
+    # need no_grad because the outer-most no_grad context manager does not affect
+    # operation inside sub-thread
+    with torch.no_grad():
+        # step 1 map pull_grad_list data to client's device
+
+        transfered_grad_list = [
+            [tensor.to(client.device) for tensor in tensors] for tensors in pull_grad_list
+        ]
+
+        # step 2, client pull to update its model to latest
+        client.pull_model(pull_seeds_list, transfered_grad_list)
+
+        # step 3, client local update and get its result
+        client_local_update_result = client.local_update(seeds=local_update_seeds)
+
+    # move result to server device and return
+    return client_local_update_result.to(server_device)
+
+
 # TODO Make sure all client model intialized with same weight.
 # TODO Support Gradient Pruning
 class CeZO_Server:
@@ -63,6 +100,7 @@ class CeZO_Server:
         self,
         clients: Sequence[AbstractClient],
         device: torch.device,
+        args,
         num_sample_clients: int = 10,
         local_update_steps: int = 10,
     ) -> None:
@@ -70,6 +108,7 @@ class CeZO_Server:
         self.device = device
         self.num_sample_clients = num_sample_clients
         self.local_update_steps = local_update_steps
+        self.args = args
 
         self.seed_grad_records = SeedAndGradientRecords()
         self.client_last_updates = [0 for _ in range(len(self.clients))]
@@ -117,19 +156,65 @@ class CeZO_Server:
         seeds = [random.randint(0, 1000000) for _ in range(self.local_update_steps)]
 
         # Step 1 & 2: pull model and local update
-        step_train_loss, step_train_accuracy, local_grad_scalar_list = execute_sampled_clients(
-            self, sampled_client_index, seeds, parallel=False
-        )
+        local_grad_scalar_list: list[list[torch.Tensor]] = []  # Clients X Local_update
+        step_train_loss = Metric("Step train loss")
+        step_train_accuracy = Metric("Step train accuracy")
 
-        # Step 3: server-side aggregation and update the records
+        client_results = []
         for index in sampled_client_index:
+            client = self.clients[index]
+            last_update_iter = self.client_last_updates[index]
+            # The seed and grad in last_update_iter is fetched as well
+            # Note at that iteration, we just reset the client model so that iteration
+            # information is needed as well.
+            seeds_list = self.seed_grad_records.fetch_seed_records(last_update_iter)
+            grad_list = self.seed_grad_records.fetch_grad_records(last_update_iter)
+
+            client_results.append(
+                parallalizable_client_job(client, seeds_list, grad_list, seeds, self.device)
+            )
+
+        for index, client_local_update_result in zip(sampled_client_index, client_results):
+            step_train_loss.update(client_local_update_result.step_loss)
+            step_train_accuracy.update(client_local_update_result.step_accuracy)
+            local_grad_scalar_list.append(client_local_update_result.grad_tensors)
             self.client_last_updates[index] = iteration
 
-        avg_grad_scalar: list[torch.Tensor] = []
-        for each_client_update in zip(*local_grad_scalar_list):
-            avg_grad_scalar.append(sum(each_client_update).div_(self.num_sample_clients))
+        # Step 3: byzantine attack
+        if self.args.byz_type == "no_byz":
+            local_grad_scalar_list = no_byz(local_grad_scalar_list)
+        elif self.args.byz_type == "gaussian":
+            local_grad_scalar_list = gaussian_attack(local_grad_scalar_list, self.args.num_byz)
+        elif self.args.byz_type == "sign":
+            local_grad_scalar_list = sign_attack(local_grad_scalar_list, self.args.num_byz)
+        elif self.args.byz_type == "trim":
+            local_grad_scalar_list = trim_attack(local_grad_scalar_list, self.args.num_byz)
+        elif self.args.byz_type == "krum":
+            local_grad_scalar_list = krum_attack(
+                local_grad_scalar_list, self.args.num_byz, self.args.lr
+            )
+        else:
+            raise Exception(
+                "byz_type should be one of no_byz, gaussian, sign, trim, krum."
+                + f"But get {self.args.byz_type}"
+            )
 
-        self.seed_grad_records.add_records(seeds=seeds, grad=avg_grad_scalar)
+        # Step 4: server-side aggregation
+        if self.args.aggregation == "mean":
+            grad_scalar = mean(self.args.num_sample_clients, local_grad_scalar_list)
+        elif self.args.aggregation == "median":
+            grad_scalar = median(local_grad_scalar_list)
+        elif self.args.aggregation == "trim":
+            grad_scalar = trim(self.args.num_sample_clients, local_grad_scalar_list)
+        elif self.args.aggregation == "krum":
+            grad_scalar = krum(local_grad_scalar_list)
+        else:
+            raise Exception(
+                "aggregation type should be one of mean, median, trim, krum. "
+                + f"But get {self.args.aggregation}"
+            )
+
+        self.seed_grad_records.add_records(seeds=seeds, grad=grad_scalar)
 
         # Optional: optimize the memory. Remove is exclusive, i.e., the min last updates
         # information is still kept.
@@ -143,7 +228,7 @@ class CeZO_Server:
                 self.optim,
                 self.random_gradient_estimator,
                 seeds,
-                avg_grad_scalar,
+                grad_scalar,
             )
 
         return step_train_loss.avg, step_train_accuracy.avg
@@ -151,10 +236,6 @@ class CeZO_Server:
     def eval_model(self, test_loader: Iterable[Any]) -> tuple[float, float]:
         if self.server_model is None:
             raise RuntimeError("set_server_model_and_criterion for server first.")
-        assert self.random_gradient_estimator
-        assert self.server_criterion
-        assert self.server_accuracy_func
-
         self.server_model.eval()
         eval_loss = Metric("Eval loss")
         eval_accuracy = Metric("Eval accuracy")
