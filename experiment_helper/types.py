6749e281bb8d012e1cca3c1b4aa71cbab5aeba8f
ZidongLiu <z.liu@insidepetroleum.com>
2024-12-08 10:00:54 -0600

    split and recombine

--- experiment_helper/types.py
+++ experiment_helper/types.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+
 class LargeModel(Enum):
     opt_125m = "opt-125m"
     opt_350m = "opt-350m"
@@ -9,10 +10,12 @@ class LargeModel(Enum):
     opt_13b = "opt-13b"
     opt_30b = "opt-30b"
 
+
 class ModelDtype(Enum):
     float32 = "float32"
     float16 = "float16"
     bfloat16 = "bfloat16"
+
 
 class Dataset(Enum):
     # TODO: split 2 different type of dataset
@@ -34,6 +37,7 @@ class Dataset(Enum):
     squad = "squad"
     drop = "drop"
     xsum = "xsum"
+
 
 class RandomGradEstimateMethod(Enum):
     rge_central = "rge-central"
--- test_args.py
+++ test_args.py
@@ -1,4 +1,27 @@
-from experiment_helper.cli_parser import Settings
+from experiment_helper.cli_parser import (
+    GeneralSetting,
+    DataSetting,
+    LoraSetting,
+    ModelSetting,
+    OptimizerSetting,
+    DeviceSetting,
+    RGESetting,
+    FederatedLearningSetting,
+)
+
+
+class Settings(
+    GeneralSetting,
+    DataSetting,
+    LoraSetting,
+    OptimizerSetting,
+    DeviceSetting,
+    RGESetting,
+    FederatedLearningSetting,
+    ModelSetting,
+):
+    pass
+
 
 if __name__ == "__main__":
     args = Settings()
