cee935c8720d1be0929cd2c587c92af2c1596950
ZidongLiu <z.liu@insidepetroleum.com>
2024-12-07 19:55:21 -0600

    WIP

--- experiment_helper/cli_parser.py
+++ experiment_helper/cli_parser.py
@@ -1,7 +1,7 @@
-from pydantic import Field
+from pydantic import Field, AliasChoices
 
-from pydantic_settings import BaseSettings, SettingsConfigDict
-from experiment_helper.typing import LargeModel, ModelDtype, Dataset, RandomGradEstimateMethod
+from pydantic_settings import BaseSettings, SettingsConfigDict, CliImplicitFlag
+from experiment_helper.types import LargeModel, ModelDtype, Dataset, RandomGradEstimateMethod
 
 # class Lora(BaseSettings):
 #     '''LoRA related fields'''
@@ -11,48 +11,59 @@ from experiment_helper.typing import LargeModel, ModelDtype, Dataset, RandomGrad
 # 	lora_alpha: int = Field(default=16)
 
 
-class Settings(BaseSettings):
+class Settings(BaseSettings, cli_parse_args=True):
     # seed
     seed: int = Field(default=365)
     # LoRA
-    lora: bool = Field(default=False)
-    lora_r: int = Field(default=8)
-    lora_alpha: int = Field(default=16)
+    lora: CliImplicitFlag[bool] = Field(default=False)
+    lora_r: int = Field(default=8, validation_alias=AliasChoices("lora-r"))
+    lora_alpha: int = Field(default=16, validation_alias=AliasChoices("lora-alpha"))
     # data
     dataset: Dataset = Field(default=Dataset.mnist.value)
-    train_batch_size: int = Field(default=8)
-    test_batch_size: int = Field(default=8)
-    iid: bool = Field(default=True)
-    dirichlet_alpha: float = Field(default=1.0)
-    num_workers: int = Field(default=2)
+    train_batch_size: int = Field(default=8, validation_alias=AliasChoices("train-batch-size"))
+    test_batch_size: int = Field(default=8, validation_alias=AliasChoices("test-batch-size"))
+    iid: CliImplicitFlag[bool] = Field(default=True)
+    dirichlet_alpha: float = Field(default=1.0, validation_alias=AliasChoices("dirichlet-alpha"))
+    num_workers: int = Field(default=2, validation_alias=AliasChoices("num-workers"))
     # model
-    large_model: LargeModel = Field(default=LargeModel.opt_125m.value)
-    model_dtype: ModelDtype = Field(default=ModelDtype.float32.value)
+    large_model: LargeModel = Field(
+        default=LargeModel.opt_125m.value, validation_alias=AliasChoices("large-model")
+    )
+    model_dtype: ModelDtype = Field(
+        default=ModelDtype.float32.value, validation_alias=AliasChoices("model-dtype")
+    )
     # optimizer
     lr: float = Field(default=1e-4)
     momentum: float = Field(default=0)
     # device
-    no_cuda: bool = Field(default=False)
-    no_mps: bool = Field(default=False)
+    no_cuda: CliImplicitFlag[bool] = Field(default=False, validation_alias=AliasChoices("no-cuda"))
+    no_mps: CliImplicitFlag[bool] = Field(default=False, validation_alias=AliasChoices("no-mps"))
     # non-fl training loop
     epoch: int = Field(default=500)
-    warmup_epochs: int = Field(default=5)
-    log_to_tensorboard: str | None = Field(default=None)
+    warmup_epochs: int = Field(default=5, validation_alias=AliasChoices("warmup-epochs"))
+    log_to_tensorboard: str | None = Field(
+        default=None, validation_alias=AliasChoices("log-to-tensorboard")
+    )
     # zo_grad_estimator
     mu: float = Field(default=1e-3)
-    num_pert: int = Field(default=1)
-    adjust_perturb: bool = Field(default=False)
-    grad_estimate_method: RandomGradEstimateMethod = Field(
-        default=RandomGradEstimateMethod.rge_central.value
+    num_pert: int = Field(default=1, validation_alias=AliasChoices("num-pert"))
+    adjust_perturb: CliImplicitFlag[bool] = Field(
+        default=False, validation_alias=AliasChoices("adjust-perturb")
     )
-    no_optim: bool = Field(default=False)
+    grad_estimate_method: RandomGradEstimateMethod = Field(
+        default=RandomGradEstimateMethod.rge_central.value,
+        validation_alias=AliasChoices("grad-estimate-method"),
+    )
+    no_optim: CliImplicitFlag[bool] = Field(
+        default=False, validation_alias=AliasChoices("no-optim")
+    )
     # Federated Learning
     iterations: int = Field(default=100)
-    eval_iterations: int = Field(default=20)
-    num_clients: int = Field(default=8)
-    num_sample_clients: int = Field(default=2)
-    local_update_steps: int = Field(default=1)
+    eval_iterations: int = Field(default=20, validation_alias=AliasChoices("eval-iterations"))
+    num_clients: int = Field(default=8, validation_alias=AliasChoices("num-clients"))
+    num_sample_clients: int = Field(default=2, validation_alias=AliasChoices("num-sample-clients"))
+    local_update_steps: int = Field(default=1, validation_alias=AliasChoices("local-update-steps"))
     # Byzantinem TODO improve options
     aggregation: str = Field(default="mean")  # "mean, median, trim, krum"
-    byz_type: str = Field(default="no_byz")
-    num_byz: int = Field(default=1)
+    byz_type: str = Field(default="no_byz", validation_alias=AliasChoices("byz-type"))
+    num_byz: int = Field(default=1, validation_alias=AliasChoices("num-byz"))
---
+++ test_args.py
@@ -0,0 +1,5 @@
+from experiment_helper.cli_parser import Settings
+
+if __name__ == "__main__":
+    args = Settings()
+    print(args)
