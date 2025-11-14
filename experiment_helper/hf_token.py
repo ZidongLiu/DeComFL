13669db9dff973384aceda59fa88a428ad4caabd
Zidong Liu <zidong.liu.1210@gmail.com>
2025-11-14 16:11:25 -0600

    use env file

--- experiment_helper/hf_token.py
+++ experiment_helper/hf_token.py
@@ -1,8 +1,4 @@
-# import dotenv
-# import os
-# dotenv.load_dotenv()
-# HF_TOKEN = os.getenv("hf_token")
-part1 = "hf_fMAy"
-part2 = "yekwcIPGkcjn"
-part3 = "KzpXdIldvVvMjGRzjL"
-HF_TOKEN = part1 + part2 + part3
+import dotenv
+import os
+dotenv.load_dotenv()
+HF_TOKEN = os.getenv("hf_token")
