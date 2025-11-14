b55a9f9c542ec5353b57a0185618954cb1a989f9
ZidongLiu <z.liu@insidepetroleum.com>
2025-11-13 18:53:23 -0600

    change

--- experiment_helper/hf_token.py
+++ experiment_helper/hf_token.py
@@ -2,4 +2,7 @@
 # import os
 # dotenv.load_dotenv()
 # HF_TOKEN = os.getenv("hf_token")
-HF_TOKEN = "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"
+part1 = "hf_fMAy"
+part2 = "yekwcIPGkcjn"
+part3 = "KzpXdIldvVvMjGRzjL"
+HF_TOKEN = part1 + part2 + part3
