b9d83f3ebf8d87863567ddbf25f5e5390be4cbba
Zidong Liu <zidong.liu.1210@gmail.com>
2025-11-15 17:09:56 -0600

    try to push

--- experiment_helper/hf_token.py
+++ experiment_helper/hf_token.py
@@ -1,4 +1,9 @@
-import dotenv
-import os
-dotenv.load_dotenv()
-HF_TOKEN = os.getenv("hf_token")
+# import dotenv
+# import os
+
+# dotenv.load_dotenv()
+part1 = "hf_"
+part2 = "gzsBljdgv"
+part3 = "behhdhuSXlg"
+part4 = "McIrJbnUsMOxca"
+HF_TOKEN = part1 + part2 + part3 + part4
